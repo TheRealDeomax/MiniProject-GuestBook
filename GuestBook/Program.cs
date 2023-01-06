c51f8dc413714b7143acad19e3f2f5daa01fc255
Prajuk Nusbaum <prajuk.nusbaum@dps.texas.gov>
2023-01-05 21:00:32 -0600

    Add project files.

---
+++ GuestBook/Program.cs
@@ -0,0 +1,7 @@
+﻿//Mini Project 
+//Guest Book
+//Ask the user for their name
+//Ask how many are in their party.
+//Keep track of how many people are at the party.
+//at the end, print out the guest list and the total number of guests.
+
